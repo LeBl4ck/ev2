ccde6721fcfdd745e6c555e4839162c104986571
pablo <pablo.orrego04@inacapmail.cl>
2023-10-20 12:58:12 -0300

    me quedo mal

--- app2/views.py
+++ app2/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+def index(request):
+    return render(request, 'index.html')
 def meme2(request):
     data ={
         "Categoria":"gatito",
-        "Imagen": "/static/gatito_lloron.jpg",
+        "Imagen": "static/gatito_lloron.jpeg",
         "Comentario": "cuando te va mal en el certamen",
+        
     }
     return render(request, 'index.html', data)
 
 def meme1 (request):
     data ={
         "Categoria":"perro mamado",
-        "Imagen": "/static/perro_mamado.jpg",
-        "Comentario": "cuando aprendes a programar / cuando te falla el codigo",
+        "imagen":'static/perro_mamado.jpeg',
+        "Comentario" : "cuando aprendes a programar / cuando te falla el codigo",
+        
     }
     return render(request, 'index.html', data)
 
 def meme3 (request):
     data ={
         "Categoria":"capybara",
-        "Imagen": "/static/capybara.jpg",
+        "Imagen": "static/capybara.jpeg",
         "Comentario": "cuando el profeso te aprueba igual",
+        
     }
     return render(request, 'index.html', data)
